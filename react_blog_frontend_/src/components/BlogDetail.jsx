09ed96c5a0c8b6cb7f76136757108e11c5b1fc87
Soweto25 <157810846+Soweto25@users.noreply.github.com>
2024-06-26 14:24:08 +0100

    refactor: added comments for readability

--- react_blog_frontend_/src/components/BlogDetail.jsx
+++ react_blog_frontend_/src/components/BlogDetail.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react' // Added missing imports
+// This is the blog post detail component
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const BlogDetail = () => {
